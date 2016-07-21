69da6f5f612e0162d535bd2e1f2d4579c994efb2
Sandro Neumann <sandro.neumann@idgis.nl>
2016-07-21 14:59:17 +0200

    added json rest interface

--- both/router.js
+++ both/router.js
@@ -40,3 +40,66 @@ Router.route('/coupling/ontwerp/:_id', function () {
 }, {
     name: 'couplingontwerpedit'
 });
+
+Router.map(function () {
+	this.route('text_json', {
+		path: '/text/json',
+	    where: 'server',
+	    action: function () {
+	      var texts = Text.find();
+	      var newTexts = [];
+	      
+	      texts.forEach(function(text) {
+	    	 var html = text.content;
+	    	 
+	    	 var i = -1;
+	    	 var images = [];
+	    	 while ((i = html.indexOf('<img', i + 1)) != -1){
+	    	   var indexEnd = html.indexOf('>', i) + 1;
+	    	   images.push(html.substring(i, indexEnd));
+	    	   
+	    	   var html = html.replace(html.substring(i, indexEnd), '');
+	    	 }
+	    	 
+	    	 newTexts.push(
+	    	   {
+	    		   id: text._id,
+	    		   name: text.name,
+	    		   texttype: text.texttype,
+	    		   html: html,
+	    		   images: images
+	    	   }
+	    	 );
+	      });
+	      
+	      this.response.setHeader('Content-Type', 'application/json');
+	      this.response.end(EJSON.stringify(newTexts, {indent: true}));
+	  }
+	});
+});
+
+Router.map(function () {
+	this.route('coupling_leidend_json', {
+		path: '/coupling/leidend/json',
+	    where: 'server',
+	    action: function () {
+	      var coupling = CouplingLeidend.find().fetch();
+	      
+	      this.response.setHeader('Content-Type', 'application/json');
+	      this.response.end(EJSON.stringify(coupling, {indent: true}));
+	  }
+	});
+});
+
+Router.map(function () {
+	this.route('coupling_ontwerp_json', {
+		path: '/coupling/ontwerp/json',
+	    where: 'server',
+	    action: function () {
+	      var coupling = CouplingOntwerp.find().fetch();
+	      
+	      this.response.setHeader('Content-Type', 'application/json');
+	      this.response.end(EJSON.stringify(coupling, {indent: true}));
+	  }
+	});
+});
